47cd857070b0049cdd41559e0d8efcdcec381cd4
shivam satpute <shivam897satpute@gmail.com>
2021-05-17 22:13:47 +0530

    [shivam]add: Create a Board

---
+++ TicTacToeGame/TicTacToe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {
+        public static char[] board;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe");
+
+            createBoard();
+        }
+
+        private static char[] createBoard()
+        {
+             board = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+    
+}
+
