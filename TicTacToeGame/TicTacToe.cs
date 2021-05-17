5321de0a3155e8e68c3396d0e5e2523687074df2
shivam satpute <shivam897satpute@gmail.com>
2021-05-17 22:18:41 +0530

    [shivam]add: Create a Board

--- TicTacToeGame/TicTacToe.cs
+++ TicTacToeGame/TicTacToe.cs
@@ -8,20 +8,40 @@ namespace TicTacToeGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to TicTacToe");
-
             createBoard();
+            choice();
         }
 
         private static char[] createBoard()
         {
-             board = new char[10];
+            board = new char[10];
             for (int i = 0; i < 10; i++)
             {
                 board[i] = ' ';
             }
             return board;
         }
+
+        public static void choice()
+        {
+
+            Console.WriteLine("Enter the choice \nX \n0");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "X":
+                    Console.WriteLine("your choice is  X");
+                    break;
+                case "0":
+                    Console.WriteLine("your choice is 0");
+                    break;
+                default:
+                    Console.WriteLine("select valid option");
+                    break;
+
+            }
+        }
+
     }
-    
 }
 
