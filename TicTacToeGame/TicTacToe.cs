9f3dccb39e45dfdc97de1cae73eeba07dd201610
shivam satpute <shivam897satpute@gmail.com>
2021-05-17 22:23:54 +0530

    [shivam]add: Show a Board

--- TicTacToeGame/TicTacToe.cs
+++ TicTacToeGame/TicTacToe.cs
@@ -5,11 +5,13 @@ namespace TicTacToeGame
     class TicTacToe
     {
         public static char[] board;
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to TicTacToe");
             createBoard();
             choice();
+            Board();
         }
 
         private static char[] createBoard()
@@ -41,7 +43,30 @@ namespace TicTacToeGame
 
             }
         }
+        private static void Board()
 
+        {
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
+
+        }
     }
+
 }
 
